43dd5b512b6b5046457690ec7fb0c1f3d68dc30c
Viorel Stefan <iviostefan@gmail.com>
2021-04-30 22:35:31 +0300

    click item random first choice

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-})
+document.addEventListener("DOMContentLoaded", function () {
+  let game = document.getElementById("game");
+  let options = ["x", "0"];
+  let firstChoice = Math.floor(Math.random() * 2);
+
+  class Item {
+    render() {
+      let btn = document.createElement("button");
+      btn.setAttribute("class", "child");
+      btn.addEventListener("click", function () {
+        btn.innerText = options[firstChoice];
+      });
+      game.appendChild(btn);
+    }
+  }
+
+  //render game
+  for (let i = 0; i < 9; i++) {
+    let obj = new Item();
+    obj.render();
+  }
+});
