debd4405b2d37de67fe85bc8a902888669a7d9d5
Viorel Stefan <iviostefan@gmail.com>
2021-05-01 12:48:31 +0300

    paint gameover type 1 or 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let game = document.getElementById("game");
+ // let game = document.getElementById("game");
+  items = document.getElementById("game").children;
   //let options = ["x", "0"];
   //let firstChoice = Math.floor(Math.random() * 2);
   let play = [];
@@ -12,7 +13,28 @@ document.addEventListener("DOMContentLoaded", function () {
   let dp = [];
   let ds = [];
 
-  items = document.getElementById("game").children;
+  let paint = (what) => {
+    for(let i = 0; i < what.length; i++) {
+      what[i].style.color = "#fff";
+      what[i].style.backgroundColor = "#a79e9e";
+    }
+  }
+
+  let GameOver = (type) => {
+    for (let i = 0; i < items.length; i++) {
+      items[i].setAttribute("disabled", "true");
+    }
+    switch(type) {
+      case 1:
+        paint(dp);
+        break;
+      case 2:
+        paint(ds);
+        break;
+    }
+  }
+
+
   for (let i = 0; i < items.length; i++) {
     items[i].addEventListener("click", function () {
       if (!items[i].classList.contains("pressed")) {
@@ -28,27 +50,27 @@ document.addEventListener("DOMContentLoaded", function () {
         }
 
         if (items[i].id[0] == 0 && items[i].id[1] == 2) {
-          ds.push(items[i].innerText);
+          ds.push(items[i]);
         }
         if (items[i].id[0] == 1 && items[i].id[1] == 1) {
-          ds.push(items[i].innerText);
+          ds.push(items[i]);
         }
         if (items[i].id[0] == 2 && items[i].id[1] == 0) {
-          ds.push(items[i].innerText);
+          ds.push(items[i]);
         }
 
         if (items[i].id[0] === items[i].id[1]) {
-          dp.push(items[i].innerText);
+          dp.push(items[i]);
         }
         console.log(dp);
         if (dp.length === 3) {
-          if (dp[0] === dp[1] && dp[1] == dp[2]) {
-            console.log("ai castigat pe diagolana princiaplaa");
+          if (dp[0].innerText === dp[1].innerText && dp[1].innerText == dp[2].innerText) {
+            GameOver(1);
           }
         }
         if (ds.length === 3) {
-          if (ds[0] === ds[1] && ds[1] == ds[2]) {
-            console.log("ai castigat pe diagolana secundara");
+          if (ds[0].innerText === ds[1].innerText && ds[1].innerText == ds[2].innerText) {
+            GameOver(2);
           }
         }
         finish++;
