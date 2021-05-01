ed7055c0d04b32ef4b0f2400b75e7a1d8a02a441
Viorel Stefan <iviostefan@gmail.com>
2021-05-01 12:29:36 +0300

    win check corner

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   let game = document.getElementById("game");
-  let options = ["x", "0"];
-  let firstChoice = Math.floor(Math.random() * 2);
+  //let options = ["x", "0"];
+  //let firstChoice = Math.floor(Math.random() * 2);
   let play = [];
-  let click = false;
-  let newGame = true;
+ // let click = false;
+ // let newGame = true;
+  let finish = 0;
 
-
-  console.log(newGame);
-  class Item {
-    constructor(id) {
-      this.id = id;
-    }
-    render() {
-      //let click = false;
-      let btn = document.createElement("button");
-      btn.setAttribute("class", "child");
-      btn.setAttribute("id", this.id);
-/*       btn.addEventListener("click", function () {
-        if (newGame) {
-          //   click = true;
-          btn.innerText = options[firstChoice];
-          play.push(options[firstChoice]);
-          newGame = false;
-        }
-        console.log(newGame);
-
-        btn.classList.add("pressed");
-      }); */
-      game.appendChild(btn);
-    }
-  }
-
-  //render game
-  for (let i = 0; i < 9; i++) {
-    let obj = new Item(i);
-    obj.render();
-  }
+ // let rows = [];
+ // let cols = [];
+  let dp = [];
+  let ds = [];
 
   items = document.getElementById("game").children;
   for (let i = 0; i < items.length; i++) {
@@ -45,10 +19,44 @@ document.addEventListener("DOMContentLoaded", function () {
         items[i].classList.add("pressed");
         if (play.pop() === "x") {
           items[i].innerText = "0";
+          items[i].classList.add("0");
           play.push("0");
         } else {
           items[i].innerText = "x";
+          items[i].classList.add("x");
           play.push("x");
+        }
+
+        if (items[i].id[0] == 0 && items[i].id[1] == 2) {
+          ds.push(items[i].innerText);
+        }
+        if (items[i].id[0] == 1 && items[i].id[1] == 1) {
+          ds.push(items[i].innerText);
+        }
+        if (items[i].id[0] == 2 && items[i].id[1] == 0) {
+          ds.push(items[i].innerText);
+        }
+
+        if (items[i].id[0] === items[i].id[1]) {
+          dp.push(items[i].innerText);
+        }
+        console.log(dp);
+        if (dp.length === 3) {
+          if (dp[0] === dp[1] && dp[1] == dp[2]) {
+            console.log("ai castigat pe diagolana princiaplaa");
+          }
+        }
+        if (ds.length === 3) {
+          if (ds[0] === ds[1] && ds[1] == ds[2]) {
+            console.log("ai castigat pe diagolana secundara");
+          }
+        }
+        finish++;
+      }
+      if (finish === 9) {
+        console.log("finish game");
+        for (let i = 0; i < items.length; i++) {
+          items[i].setAttribute("disabled", "true");
         }
       }
     });
